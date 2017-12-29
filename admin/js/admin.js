ab7aec2f5f6df7629d29a0b97aa2a0480c229994
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-12-29 09:37:15 +0800

    Added toggleIt function, fixed to closing rawa-fields on widget save.

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,6 +1,17 @@
 (function($){
+  function toggleIt() {
     $('body').on('click', '.rawa-toggle', function(e){
-		$(this).toggleClass('open');
-		$('.rawa-field').toggle();
+      $(this).toggleClass('open');
+      $('.rawa-field').toggle();
+      e.preventDefault();
     });
+  }
+
+  $(document).on('widget-updated', function(event, widget){
+    $(widget).each(function(){
+      $('.rawa-field').toggle();
+    });
+  });
+
+  toggleIt();
 })(jQuery);
