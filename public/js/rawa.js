534d7c99f5e327f5270a30591d748ac6ee6c74bd
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-07-27 08:17:08 +0800

    Removed unneeded lines of code

--- public/js/rawa.js
+++ public/js/rawa.js
@@ -28,25 +28,7 @@
         easing: rawa_aos.easing,
         delay: parseInt(rawa_aos.delay),
         anchor: rawa_aos.anchor,
-        disable: function() {
-            switch(rawa_aos.disable) {
-                case 'phone':
-                    return 'phone';
-                    break;
-                case 'mobile':
-                default:
-                    return 'mobile';
-                    break;
-                case 'tablet':
-                    return 'tablet';
-                    break;
-                case 'custom':
-                    var maxWidth = parseInt(rawa_aos.custom);
-                    return window.innerWidth < maxWidth;
-                    break;
-            }
-        },
         disable: $disable,
-        once: (rawa_aos.once == "true"),
+        once: (rawa_aos.once == "true")
     });
 })(jQuery);
