ed8a684b7462ac715ffc6dd9c906230430891284
webdevsuperfast <acob.rotsen@hotmail.com>
2026-02-23 04:14:04 +0800

    Build output

--- admin/js/rawa-admin.js
+++ admin/js/rawa-admin.js
@@ -1,25 +1,25 @@
-(function ($) {
-	$( document ).on(
-		"click",
-		".rawa-toggle",
-		function (e) {
-			e.preventDefault();
-			const $toggler = $( this );
-			const $next    = $toggler.next();
-			$toggler.toggleClass( "open" );
-			$next.toggle();
-			localStorage.setItem( "rawaDisplay", $next.is( ":visible" ) );
-		}
-	);
-	$( document ).on(
-		"widget-updated widget-added",
-		function (event, widget) {
-			const $toggler = $( widget ).find( ".rawa-toggle" );
-			const display  = localStorage.getItem( "rawaDisplay" );
-			if (display === "true") {
-				$toggler.addClass( "open" );
-				$toggler.next().show();
-			}
-		}
-	);
-})( jQuery );
+(function($) {
+  $(document).on(
+    "click",
+    ".rawa-toggle",
+    function(e) {
+      e.preventDefault();
+      const $toggler = $(this);
+      const $next = $toggler.next();
+      $toggler.toggleClass("open");
+      $next.toggle();
+      localStorage.setItem("rawaDisplay", $next.is(":visible"));
+    }
+  );
+  $(document).on(
+    "widget-updated widget-added",
+    function(event, widget) {
+      const $toggler = $(widget).find(".rawa-toggle");
+      const display = localStorage.getItem("rawaDisplay");
+      if (display === "true") {
+        $toggler.addClass("open");
+        $toggler.next().show();
+      }
+    }
+  );
+})(jQuery);
